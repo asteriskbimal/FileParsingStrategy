6db3618b410a3e25e449b008844c0f9857bbe2b7
Bimal <bimal.sharma2@outlook.com>
2018-12-02 13:03:10 -0500

    Update GenericDao.java

--- src/main/java/com/files/to/couchbase/repository/GenericDao.java
+++ src/main/java/com/files/to/couchbase/repository/GenericDao.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public interface GenericDao<T> {
 
-    T upsert(T entity);
+    Mono<T> upsert(T entity);
 
-     List<T> upsertAll(List<T> entities);
+    Flux<T> upsertAll(List<T> entities);
 
-    T get(String bucketKey,Class<T> entity);
+    Mono<T> get(String bucketKey,Class<T> entity);
 
-    List<T> getAll(T entity);
-    
-     List<T> getRecordsWithQueryParams(Class<T> entity, Map<String,String> queryParams);
+    Flux<T> getAll(T entity);
+
+    Flux<T> getRecordsWithQueryParams(Class<T> entity, Map<String,String> queryParams);
 
 }
